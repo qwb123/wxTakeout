cd50b23f7bec5a86dfe57b28e023f41c2240eead
SpringR <573591258@qq.com>
2018-08-09 14:16:09 +0800

    开始SpringBoot点餐系统

--- src/main/java/com/qwb/takeout/TakeoutApplication.java
+++ src/main/java/com/qwb/takeout/TakeoutApplication.java
@@ -1,9 +1,11 @@
 package com.qwb.takeout;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.qwb.takeout.dao")
 public class TakeoutApplication {
 
     public static void main(String[] args) {
