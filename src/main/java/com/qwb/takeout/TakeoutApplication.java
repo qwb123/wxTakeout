f5da213ef6b37ba5a337cbe2bf4da8127274793c
SpringR <573591258@qq.com>
2018-08-09 13:12:09 +0800

    开始SpringBoot点餐系统

---
+++ src/main/java/com/qwb/takeout/TakeoutApplication.java
@@ -0,0 +1,12 @@
+package com.qwb.takeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeoutApplication.class, args);
+    }
+}
