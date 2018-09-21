39f36cbd63e8742b98116d8c2552ee3855749fe9
SpringR <573591258@qq.com>
2018-09-21 16:53:15 +0800

    开启redis

--- src/main/java/com/qwb/takeout/TakeoutApplication.java
+++ src/main/java/com/qwb/takeout/TakeoutApplication.java
@@ -3,9 +3,11 @@ package com.qwb.takeout;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.qwb.takeout.dao")
+@EnableCaching
 public class TakeoutApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/qwb/takeout/serviceImpl/ProductInfoServiceImpl.java
+++ src/main/java/com/qwb/takeout/serviceImpl/ProductInfoServiceImpl.java
@@ -4,9 +4,7 @@ import com.qwb.takeout.dao.ProductInfoMapper;
 import com.qwb.takeout.model.entity.ProductInfo;
 import com.qwb.takeout.service.ProductInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
